a77e48d9fc5de294e20d7bf424b073416cfa3e37
GK <gopinath18k@gmail.com>
2026-08-19 11:57:36 +0530

    Sky Kapture Public Folder Files Created On 19-08-2026

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'Live',
+    emptyOutDir: true,
+    assetsDir: 'static',
+    sourcemap: false,
+  },
+  // Configure esbuild to treat both .js and .jsx files in src as JSX
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: 'load-js-files-as-jsx',
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => {
+              return {
+                loader: 'jsx',
+                contents: await fs.promises.readFile(args.path, 'utf8'),
+              };
+            });
+          },
+        },
+      ],
+    },
+  },
+});
